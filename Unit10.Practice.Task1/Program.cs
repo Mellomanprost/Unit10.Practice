6b53ac4d3dcae04345b11d11d12b95d3e66185e7
Mellomanprost <anoppa@mail.ru>
2022-10-23 13:53:55 +0300

    Добавьте файлы проекта.

---
+++ Unit10.Practice.Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10.Practice.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
