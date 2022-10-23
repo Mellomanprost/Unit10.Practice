7d3e9ba37dffc2f27fd79b60e78542a69814e894
Mellomanprost <anoppa@mail.ru>
2022-10-23 14:51:29 +0300

    Added first Task

--- Unit10.Practice.Task1/Program.cs
+++ Unit10.Practice.Task1/Program.cs
@@ -8,8 +8,58 @@ namespace Unit10.Practice.Task1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            bool flagForExit = false;
+            Console.WriteLine("Программа для суммирования двух чисел.");
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введите первое число: ");
+
+                    var a = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число: ");
+
+                    var b = double.Parse(Console.ReadLine());
+
+                    Calculator calculator = new Calculator();
+                    Console.WriteLine("{0} + {1} = {2}", a, b, calculator.Calculate(a, b));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    Console.WriteLine("Чтобы выйти напишите - выход.\nДля продолжения нажмите Enter...");
+                    var userString = Console.ReadLine();
+                    if (userString == "выход" || userString == "Выход")
+                    {
+                        flagForExit = true;
+                    }
+                }
+            } while (!flagForExit);
+
+
         }
+    }
+
+    public interface ICalculator
+    {
+        double Calculate(double x, double y);
+    }
+
+    public class Calculator : ICalculator
+    {
+
+        public double Calculate(double x, double y)
+        {
+            return x + y;
+        }
+
     }
 }
